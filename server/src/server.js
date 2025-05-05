75fdaf68ae72a98fc4a77706d9c23ae03739692a
Sai Yan Naing <pesilay2372002@gmail.com>
2025-05-05 16:24:41 +0630

    setup_database

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,16 @@
 import express from "express";
 import dotenv from "dotenv"
+import connectDB from "./lib/db.js";
+import authRoutes from "./routers/auth.routes.js";
 
 // Configuration
 dotenv.config();
 const app = express();
 
+// Routers
+app.use("/api/auth", authRoutes)
+
 app.listen(process.env.PORT, () => {
     console.log(`server is running on ${process.env.PORT}`);
+    connectDB();
 })
